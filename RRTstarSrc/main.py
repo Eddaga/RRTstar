876c6fa346b7af178695b1b9526d4104938bf2a4
Eddaga <kyuyong@esl.mju.ac.kr>
2023-11-05 21:30:45 +0900

    no. isNodeSteerOnObstacle function don't work!

--- RRTstarSrc/main.py
+++ RRTstarSrc/main.py
@@ -44,7 +44,7 @@ def main():
     start = Node(1200- 570,930, 1)
     goal = Node(1200- 610,930, 0)
     iterations = 1000
-    stepSize = 1
+    stepSize = 5
     mapMaxSize = [1200, 1200]
     possibleVelocity = 41# 150.0 km/h * 100 / 3600 = 41.16667m/s
     threshold = 10 #for isGoalReached(euclidian distance)
