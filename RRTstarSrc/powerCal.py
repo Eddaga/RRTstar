1528cf8d3846585f953c188c936920f93c209198
Eddaga <kyuyong@esl.mju.ac.kr>
2023-11-08 01:32:48 +0900

    almost done with deal on mlpParameters. have to add Powercunsumption

---
+++ RRTstarSrc/powerCal.py
@@ -0,0 +1,12 @@
+import RRTutils
+
+def mlpParameters(node1,node2):
+    velocity = (node1.velocity + node2.velocity) / 2
+    # deal accelPress as torqueMode.. so accelPress = accelration
+    accelPress = (node1.cost - node2.cost) # get accel equation
+    tilt = (node1.x - node2.y) # kind of this function to get tilts
+
+    return accelPress, velocity, tilt
+
+
+def 
