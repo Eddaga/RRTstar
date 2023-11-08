82bbcd3317e088659f1335e6123b6fff612bdd6f
Eddaga <kyuyong@esl.mju.ac.kr>
2023-11-08 19:15:24 +0900

    fix current and ParentNode

--- RRTstarSrc/powerCal.py
+++ RRTstarSrc/powerCal.py
@@ -1,10 +1,12 @@
 import RRTutils
 # import MLP~~
-def mlpParameters(node1,node2):
-    velocity = (node1.velocity + node2.velocity) / 2
+def mlpParameters(currentNode):
+    mlpParamVelocity = (currentNode.velocity + currentNode.parent.velocity) / 2
     # deal accelPress as torqueMode.. so accelPress = accelration
-    accelPress = (node1.cost - node2.cost) # get accel equation
-    tilt = (node1.x - node2.y) # kind of this function to get tilts
+    # get accel equation accel = Velocity / time!
+    accelPress = ((currentNode.velocity + currentNode.parent.velocity) / 2) / currentNode.cost - currentNode.parent.cost
+    
+    tilt = (currentNode.x - currentNode.parent.y) # kind of this function to get tilts
 
     return accelPress, velocity, tilt
 
