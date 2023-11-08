eef6749060035b0d122eda0927b2a3653fb98b9c
Eddaga <kyuyong@esl.mju.ac.kr>
2023-11-08 21:12:29 +0900

    configure getTotalPower

--- RRTstarSrc/powerCal.py
+++ RRTstarSrc/powerCal.py
@@ -1,6 +1,25 @@
 import RRTutils
 # import MLP~~
+
+def getOptimalPath(startNode, newNode):
+    optimalPath = []
+    current = newNode
+    while current.parent is not None:  # Continue until you reach the start node (which has no parent)
+        optimalPath.insert(0,current)
+        current = current.parent
+    optimalPath.insert(0,startNode)
+    print(optimalPath[0].x," ",optimalPath[1].y)
+    return optimalPath 
+
+def getTotalPower(startNode,goalNode):
+    optimalPath = getOptimalPath(startNode,goalNode)
+    
+
+    return Energy
+    
+
 def mlpParameters(currentNode):
+    optimalPath = getOptimalPath(startNode, )
     mlpParamVelocity = (currentNode.velocity + currentNode.parent.velocity) / 2
     # deal accelPress as torqueMode.. so accelPress = accelration
     # get accel equation accel = Velocity / time!
@@ -9,6 +28,8 @@ def mlpParameters(currentNode):
     tilt = (currentNode.x - currentNode.parent.y) # kind of this function to get tilts
 
     return accelPress, velocity, tilt
+
+
 
 
 
