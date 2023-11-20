44a8953bd7fe5b35243b274516e588a4aa6df210
Eddaga <kyuyong@esl.mju.ac.kr>
2023-11-21 08:06:12 +0900

    almost done...

--- RRTstarSrc/RRT.py
+++ RRTstarSrc/RRT.py
@@ -31,24 +31,6 @@ def plot_tree(nodes, newNode, mapData, goal):
     plt.pause(0.01)
 
 
-'''def plot_tree(nodes, newNode, mapData, goal):
-    plt.clf()  # 현재 피규어를 클리어하지 마세요
-    plotMap(mapData)
-    for node in nodes:
-        if node.parent is not None:
-            plt.plot([node.x, node.parent.x], [node.y, node.parent.y], 'b-', linewidth=0.5)
-    plt.plot(newNode.x, newNode.y, 'go', label='새 노드')  # 새 노드는 녹색 점으로 표시
-
-    current = newNode
-    while current.parent is not None:  # Continue until you reach the start node (which has no parent)
-        plt.plot([current.x, current.parent.x], [current.y, current.parent.y], 'r-', linewidth=1.0)
-        current = current.parent  # Move to the next node up the
-    
-    plt.pause(0.01)  # 플롯을 업데이트 하기 위해 잠시 멈춤'''
-
-    
-
-
 def rrtStar(nodes, start, stepSize, possibleVelocity, mapData, scaler, goal, threshold,binaryImage):
     hit = 0
     #for _ in range(iterations):
@@ -69,15 +51,16 @@ def rrtStar(nodes, start, stepSize, possibleVelocity, mapData, scaler, goal, thr
             hit = hit+1
             #plot_tree(nodes, newNode, mapData, goal)  # 현재 트리를 플롯
             if newNode and isGoalReached(newNode, goal, threshold):
-                #print("Goal reached!")
-                plot_tree(nodes, newNode, mapData, goal)  # 현재 트리를 플롯
+                #plot_tree(nodes, newNode, mapData, goal)  # 현재 트리를 플롯
                 print("total E = ", int(getTotalPower(start,newNode,goal)),"W")
                 print("total T = ", int(newNode.cost),"sec")
-                #print("E / T = ",int(getTotalPower(start,newNode,goal) / newNode.cost), "W/s")
-                print("")
+                print("E / T = ",int(getTotalPower(start,newNode,goal) / newNode.cost), "W/s")
 
-            if hit == 10:
+            if hit == 1000:
                 #plot_tree(nodes, newNode, mapData, goal)  # 현재 트리를 플롯
+                #print("total E = ", int(getTotalPower(start,newNode,goal)),"W")
+                #print("total T = ", int(newNode.cost),"sec")
+                #print("E / T = ",int(getTotalPower(start,newNode,goal) / newNode.cost), "W/s")
                 return nodes, hit
             
             
